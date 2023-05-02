d426d121a9edd0efe4e5e5d3843062f4904a96cd
Justinguu <justinguuu@gmail.com>
2023-05-02 16:53:04 -0400

    palindrome

---
+++ LeetCode/Easy Problems/Palindrome.js
@@ -0,0 +1,56 @@
+//9. Palindrome Number
+
+
+
+
+//QUESTION: Given an integer x, return true if x is a palindrome, and false otherwise.
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let y = x.toString().split("").reverse().join("");
+    return x === parseInt(y)
+    };
+    
+    
+    // let palin = x.toString().split("").reverse().join("");
+    // return x === parseInt(palin)
+    
+    //  let y = x.toString().split("").reverse().join("");
+    //         return x === parseInt(y) 
+    
+
+
+/*
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+ 
+
+Follow up: Could you solve it without converting the integer to a string?
+
+Accepted
+3.1M
+Submissions
+5.8M
+Acceptance Rate
+53.6%
+*/
