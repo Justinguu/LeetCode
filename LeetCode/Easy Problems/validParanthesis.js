3b800403bb363fcae180f9fd8f9396675863f36f
Justinguu <justinguuu@gmail.com>
2023-06-02 09:11:20 -0400

    review

--- LeetCode/Easy Problems/validParanthesis.js
+++ LeetCode/Easy Problems/validParanthesis.js
@@ -11,7 +11,7 @@ An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
-
+//
 //
 /**
  * @param {string} s
