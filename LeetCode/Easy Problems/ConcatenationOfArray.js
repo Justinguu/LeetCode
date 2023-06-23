ab32c6cb0684d420c802b2efc0334e1223c7c5b7
Justinguu <justinguuu@gmail.com>
2023-06-23 18:30:05 -0400

    review

--- LeetCode/Easy Problems/ConcatenationOfArray.js
+++ LeetCode/Easy Problems/ConcatenationOfArray.js
@@ -16,7 +16,7 @@ var getConcatenation = function(nums) {
 //
 
 var getConcatenation = function(nums) {
-	//spread the nums array twice and return it
+	////spread the nums array twice and return it
     return [...nums,...nums]
 };
 ////
