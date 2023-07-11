68f6b8496deb79844629f4a5ff9ef0ddb8eef0dc
Justinguu <justinguuu@gmail.com>
2023-07-11 15:37:02 -0400

    right answer revised for swapNodes

--- HackerRank/swapNodes.js
+++ HackerRank/swapNodes.js
@@ -1,4 +1,5 @@
-//Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's node(i.e., only nodes themselves may be changed)
+//Given a linked list, swap every two adjacent nodes and return its head. 
+//You must solve the problem without modifying the values in the list's node(i.e., only nodes themselves may be changed)
 
 function swapPairs(head) {
   
@@ -6,4 +7,26 @@ function swapPairs(head) {
 }
 
 
+function swapPair(head) {
+    // Create a dummy node as the previous node of the head
+    const dummy = { next: head };
+    let prev = dummy;
+  
+    while (head !== null && head.next !== null) {
+      // Nodes to be swapped
+      const firstNode = head;
+      const secondNode = head.next;
+  
+      // Swapping
+      prev.next = secondNode;
+      firstNode.next = secondNode.next;
+      secondNode.next = firstNode;
+  
+      // Update prev and head for the next pair
+      prev = firstNode;
+      head = firstNode.next;
+    }
+  
+    return dummy.next;
+  }
   
