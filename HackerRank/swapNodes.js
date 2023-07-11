d5f7a71d892f972b50902e21b00957fc185989ef
Justinguu <justinguuu@gmail.com>
2023-07-11 15:36:35 -0400

    swapPairs

---
+++ HackerRank/swapNodes.js
@@ -0,0 +1,9 @@
+//Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's node(i.e., only nodes themselves may be changed)
+
+function swapPairs(head) {
+  
+
+}
+
+
+  
