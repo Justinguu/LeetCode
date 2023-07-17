e0f3ba155486b5f912acda53f19203d37c0ff8e5
Justinguu <justinguuu@gmail.com>
2023-07-17 13:47:27 -0400

    explanation included Anagrams

--- LeetCode/Easy Problems/Valid Anagram.js
+++ LeetCode/Easy Problems/Valid Anagram.js
@@ -3,36 +3,12 @@
 242. Valid Anagram
 Easy
 9.1K
-285
 Companies
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
+//Question
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
- 
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-Output: true
-Example 2:
-
-Input: s = "rat", t = "car"
-Output: false
- 
-
-Constraints:
-
-1 <= s.length, t.length <= 5 * 104
-s and t consist of lowercase English letters.
-*/
-
-
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isAnagram = function(s, t) {
     
 };
@@ -41,8 +17,26 @@ var isAnagram = function(s, t) {
  * @param {string} s
  * @param {string} t
  * @return {boolean}
- */
+*/
+
+//ANSWER
 var isAnagram = function(s, t) {
     return s.split('').sort().join('') === t.split('').sort().join('');
 };
-///
+
+// Explanation 
+// We can simplify this by checking s and t against each other by splitting their characters.
+//Sorting the characters in order and joining them back to see if the elements are the same
+
+/*
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+*/
