db8a1d6ccf9e5877fd431e2df94e3d4c326a1003
Justinguu <justinguuu@gmail.com>
2023-11-14 14:35:00 -0500

    another solution to longest common prefix

--- LeetCode/Easy Problems/LongestCommonPrefix.js
+++ LeetCode/Easy Problems/LongestCommonPrefix.js
@@ -51,3 +51,17 @@ var longestCommonPrefix = function(strs) {
         return prev.slice(0, i);
     });
 };
+
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return ''
+
+    for (let i = 0; i <= strs[0].length; i++){
+        for (let j = 1; j < strs.length; j++){
+            if (strs[0][i] !== strs[j][i]){
+                return strs[0].slice(0,i)
+            }
+
+        }
+    }
+    return strs[0]
+};
